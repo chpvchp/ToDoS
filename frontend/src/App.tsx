e7d739e1bc03dbe73e0ef5b39105c707e80ae542
Cao Hoang Phuc <caophuc@ubuntu-26.04-caophuc-computer>
2026-06-03 14:50:20 +0700

    setup first

---
+++ frontend/src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-200 flex justify-center items-center">
+      <h1 className="p-4 bg-white border border-gray-400 rounded-lg text-lg font-sans font-bold shadow-md transition duration-200 hover:bg-blue-600 hover:text-white">Welcome to ToDoS Project</h1>
+    </div>
+  )
+}
