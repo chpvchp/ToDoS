ff25e92ce6aee884043e5974a319827b5fc37234
Cao Hoang Phuc <caophuc@ubuntu-26.04-caophuc-computer>
2026-06-04 13:14:53 +0700

    feat: add CardInput in CardTodo to input new title

--- frontend/src/components/CardTodo.tsx
+++ frontend/src/components/CardTodo.tsx
@@ -1,16 +1,39 @@
+import { useState } from "react";
 import Button from "./Button";
+import CardInput from "./CardInput";
 
 type CardTodoProps = {
   title: string;
-  onDelete: () => void
+  onDelete: () => void;
+  onRename: (newTitle: string) => void;
 }
 
-export default function CardTodo({ title, onDelete } : CardTodoProps) {
+export default function CardTodo({ title, onDelete, onRename } : CardTodoProps) {
+  const [editing, setEditing] = useState(false);
+  const [newTitle, setNewTitle] = useState("");
+
+
   return (
-    <div className="p-2 bg-white border border-gray-400 rounded-md flex flex-col gap-2">
-      <p className="p-2 border border-gray-400 rounded-md">{title}</p>
-      <Button label="Edit" onClick={() => console.log("Edit Todo")}/>
-      <Button label="Del" onClick={onDelete}/>
-    </div>
+    <>
+      {editing ? (
+        <CardInput 
+          label="Save"
+          value={newTitle}
+          onChange={(e) => setNewTitle(e.target.value)}
+          onClick={() => {
+            onRename(newTitle);
+            setEditing(false);
+            setNewTitle("");
+          }}
+        />
+      ) : (
+        <div className="p-2 bg-white border border-gray-400 rounded-md flex flex-col gap-2">
+          <p className="p-2 border border-gray-400 rounded-md">{title}</p>
+          <Button label="Edit" onClick={() => setEditing(true)}/>
+          <Button label="Del" onClick={onDelete}/>
+        </div>
+      )}
+
+    </>
   )
 }
